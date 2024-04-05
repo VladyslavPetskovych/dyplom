fbcba92085785dfc61561178ecde11e60dcea49b
Vlad <peckovychv@gmail.com>
2024-04-05 11:22:25 +0300

    fixed footer menu

--- webapp/src/component/footerMenu.jsx
+++ webapp/src/component/footerMenu.jsx
@@ -8,30 +8,18 @@ function FooterMenu() {
         <nav className="bg-black font-mono text-white h-24 absolute bottom-0 w-full">
           <div className="flex flex-row text-center h-full justify-center items-center">
             <div className="flex-grow w-1/3">
-              <Link
-                to="/"
-                className="block w-full h-full"
-                activeClassName="bg-gray-800"
-              >
-                <button>Відповідай на питання</button>
+              <Link to="/" className="block w-full h-full">
+                Відповідай на питання
               </Link>
             </div>
             <div className="flex-grow w-1/3">
-              <Link
-                to="/communities"
-                className="block w-full h-full"
-                activeClassName="bg-gray-800"
-              >
-                <button>Спільноти</button>
+              <Link to="/communities" className="block w-full h-full">
+                Спільноти
               </Link>
             </div>
             <div className="flex-grow w-1/3">
-              <Link
-                to="/profile"
-                className="block w-full h-full"
-                activeClassName="bg-gray-800"
-              >
-                <button>Профіль</button>
+              <Link to="/profile" className="block w-full h-full">
+                Профіль
               </Link>
             </div>
           </div>
