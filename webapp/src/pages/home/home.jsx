20b0560c6d127ab7e77a967bde8e9a5110b773d3
Vlad <peckovychv@gmail.com>
2024-04-26 18:50:12 +0300

    testing redux

--- webapp/src/pages/home/home.jsx
+++ webapp/src/pages/home/home.jsx
@@ -1,18 +1,20 @@
 const tg = window.Telegram.WebApp;
 import { useEffect } from "react";
 import Questions from "./questionsCard";
+import { useDispatch } from "react-redux";
+import { setChatId } from "../../../src/redux/store";
 
-import axios from "axios"
 function Home() {
+  const dispatch = useDispatch();
+  dispatch(setChatId(tg.initDataUnsafe.user.id));
   useEffect(() => {
     tg.ready();
-    
-  },[]);
+  }, []);
 
   const onClose = () => {
     tg.close();
   };
-  
+
   return (
     <div>
       <div className="h-screen text-white text-base font-oswald font-semibold [text-shadow:_0_1px_0_rgb(0_0_0_/_90%)]">
@@ -32,10 +34,16 @@ function Home() {
         </div>
 
         <Questions />
-        <pre className="mt-[1000px]">{JSON.stringify(tg.initDataUnsafe.user.id, null, 2)}</pre>
+        <pre className="mt-[1000px]">
+          {JSON.stringify(tg.initDataUnsafe.user.id, null, 2)}
+        </pre>
         <p>--------------</p>
-        <pre className="">{JSON.stringify(tg.initDataUnsafe.user, null, 2)}</pre>
-        <pre className="mt-[1000px]">{JSON.stringify(tg.initDataUnsafe, null, 2)}</pre>
+        <pre className="">
+          {JSON.stringify(tg.initDataUnsafe.user, null, 2)}
+        </pre>
+        <pre className="mt-[1000px]">
+          {JSON.stringify(tg.initDataUnsafe, null, 2)}
+        </pre>
       </div>
     </div>
   );
--- webapp/src/pages/home/question.jsx
+++ webapp/src/pages/home/question.jsx
@@ -1,9 +1,9 @@
 import Rating from "./rating";
 import React, { useState, useEffect } from "react";
-
+import { useSelector } from 'react-redux';
 function Question({ question, onNext }) {
     const [rating, setRating] = useState(null);
-  
+    const chatId = useSelector(state => state.user.chatId);
     const handleAnswer = () => {
       console.log(rating)
       onNext(rating); 
@@ -13,6 +13,8 @@ function Question({ question, onNext }) {
     return (
       <div className="flex flex-col items-center justify-center">
         <p>{question.questionText}</p>
+        <p>ChatID</p>
+        <p>{chatId}</p>
         <Rating onRatingSelect={setRating} /> 
         <button onClick={handleAnswer}>Відповісти</button>
       </div>
