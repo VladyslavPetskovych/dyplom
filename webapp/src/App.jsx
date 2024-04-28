829f91b15079ea058bb3b6e44d2610733fa5f380
Vlad <peckovychv@gmail.com>
2024-04-28 14:33:26 +0300

    testing

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     tg.ready();
     dispatch(setChatId(tg.initDataUnsafe.user.id));
-     //dispatch(setChatId(938729564));
+    //dispatch(setChatId(938729564));
   }, []);
   return (
     <BrowserRouter>
@@ -22,15 +22,20 @@ function App() {
         <Route path="/profile" element={<Profile />} />
       </Routes>
       <FooterMenu />
-      <p>{tg.initDataUnsafe.user.id}</p>
-      <pre className="mt-[1000px]">
-        {JSON.stringify(tg.initData.user, null, 2)}
-      </pre>
-      <p>--------------</p>
-      <pre className="">{JSON.stringify(tg.initDataUnsafe.user, null, 2)}</pre>
-      <pre className="mt-[1000px]">
-        {JSON.stringify(tg.initDataUnsafe, null, 2)}
-      </pre>
+
+      <div>
+        <p>{tg.initDataUnsafe.user.id}</p>
+        <pre className="mt-[1000px] text-white">
+          {JSON.stringify(tg.initData.user, null, 2)}
+        </pre>
+        <p>--------------</p>
+        <pre className="">
+          {JSON.stringify(tg.initDataUnsafe.user, null, 2)}
+        </pre>
+        <pre className="mt-[1000px]">
+          {JSON.stringify(tg.initDataUnsafe, null, 2)}
+        </pre>
+      </div>
     </BrowserRouter>
   );
 }
