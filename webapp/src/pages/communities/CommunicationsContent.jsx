96c14684e0e3789d21d257e557fee2cf9f38ad83
Vlad <peckovychv@gmail.com>
2024-05-28 19:10:29 +0300

    fixed chat list

--- webapp/src/pages/communities/CommunicationsContent.jsx
+++ webapp/src/pages/communities/CommunicationsContent.jsx
@@ -4,7 +4,7 @@ import UsersAllbut from "./usersAllbut";
 
 function CommunicationsContent({ onUsersAllbutToggle }) {
   return (
-    <div className="p-4 bg-black bg-opacity-75 rounded-xl h-full overflow-y-auto text-white">
+    <div className="p-4 bg-black bg-opacity-75 rounded-xl h-full overflow-y-auto text-white mb-24" >
       <div className="flex flex-row justify-between items-center mb-5">
         <p className="text-xl font-bold">Чати:</p>
         <UsersAllbut onToggleExpand={onUsersAllbutToggle} />
